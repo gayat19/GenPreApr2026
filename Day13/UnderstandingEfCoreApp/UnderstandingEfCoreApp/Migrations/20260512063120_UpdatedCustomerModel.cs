a23147028b7539d65a149a650633b115bf966326
unknown <gayat_19@yahoo.com>
2026-05-12 13:05:05 +0530

    Day13-EF Intro

---
+++ Day13/UnderstandingEfCoreApp/UnderstandingEfCoreApp/Migrations/20260512063120_UpdatedCustomerModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnderstandingEfCoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedCustomerModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "customers");
+        }
+    }
+}
