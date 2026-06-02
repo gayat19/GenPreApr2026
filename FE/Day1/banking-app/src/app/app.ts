8e4ce1d04980c1658429b51b55c7aaca0a4ef8db
unknown <gayat_19@yahoo.com>
2026-06-02 10:26:25 +0530

    Signal and First HTTP call

--- FE/Day1/banking-app/src/app/app.ts
+++ FE/Day1/banking-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Customers } from './customers/customers';
+import { Products } from "./products/products";
 
 @Component({
   selector: 'app-root',
-  imports: [ Customers],
+  imports: [Customers, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
