c8af3c3e5f7f4545ad27be01bd6e9a8aeb61e11a
unknown <gayat_19@yahoo.com>
2026-06-03 10:44:01 +0530

    Subject-Rxjx

--- FE/Day1/banking-app/src/app/account/account.ts
+++ FE/Day1/banking-app/src/app/account/account.ts
@@ -1,27 +1,57 @@
 import { Component } from '@angular/core';
 import { BankingApiService } from '../services/bankingapi.service';
+import { Subject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
 export class Account {
   
-  constructor(private bankingApiService: BankingApiService) {
-  }
+   searchAccountNumber: string = '';
 
-  getAccountDetails(accNumber:string){
-    this.bankingApiService.getAccountDetails(accNumber).subscribe({
-      next: (response) => {
-        console.log("Account details", response);
-        alert("Account details fetched successfully!")
+  private searchSubject = new Subject<string>();
+  
+  constructor(private bankingApiService: BankingApiService) {
+    this.searchSubject.subscribe({
+      next:(accNumber) => {
+        console.log("Fetching account details for account number:", accNumber);
       },
       error: (error) => {
         console.error("Failed to fetch account details", error);
-        alert("Failed to fetch account details. Please try again.");
+        
+      },
+      complete: () => {        
+        console.log("Account details fetched successfully!");
+      
       }
     });
+  }
+
+  // getAccountDetails(accNumber:string){
+  //   this.bankingApiService.getAccountDetails(accNumber).subscribe({
+  //     next: (response) => {
+  //       console.log("Account details", response);
+  //       alert("Account details fetched successfully!")
+  //     },
+  //     error: (error) => {
+  //       console.error("Failed to fetch account details", error);
+  //       alert("Failed to fetch account details. Please try again.");
+  //     }
+  //   });
+  // }
+
+
+
+    getAccountDetails(){
+      this.searchSubject.next(this.searchAccountNumber);
+  }
+
+  onDestroy(){
+    this.searchSubject.complete();
+    this.searchSubject.unsubscribe();
   }
 }
