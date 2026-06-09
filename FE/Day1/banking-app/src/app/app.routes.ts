9f70193cff306a763d78cebc8759b9eb52bb5dee
unknown <gayat_19@yahoo.com>
2026-06-09 09:52:50 +0530

    Routing Basics

--- FE/Day1/banking-app/src/app/app.routes.ts
+++ FE/Day1/banking-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Customers } from './customers/customers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:Customers},
+    {path:'products',loadComponent:()=>import('./products/products').then(m=>m.Products)},
+];
--- FE/Day1/banking-app/src/app/app.ts
+++ FE/Day1/banking-app/src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Customers } from './customers/customers';
-import { Products } from "./products/products";
-import { Login } from "./login/login";
-import { Account } from "./account/account";
+
 import { usernameSubject } from './rxjs/auth.operator';
+import { Navigation } from "./navigation/navigation";
 
 @Component({
   selector: 'app-root',
-  imports: [Customers, Products, Login, Account],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
